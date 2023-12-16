24d67a5d0fe34707cd2799fd2f86eeaf4d55069b
Alexander Liu <a@alexanderliu.com>
2023-12-15 22:44:39 -0800

    feat: change durable object name to game

--- apps/durable-object/src/index.ts
+++ apps/durable-object/src/index.ts
@@ -1,23 +1,23 @@
-import { Hono } from 'hono'
-export { Counter } from './counter'
+import { Hono } from "hono";
+export { Game } from "./game";
 
 type Bindings = {
-  COUNTER: DurableObjectNamespace
-}
+  GAME: DurableObjectNamespace;
+};
 
-const app = new Hono<{ Bindings: Bindings }>()
+const app = new Hono<{ Bindings: Bindings }>();
 
-app.get('*', async (c) => {
-  const id = c.env.COUNTER.idFromName('A')
-  const obj = c.env.COUNTER.get(id)
-  const resp = await obj.fetch(c.req.url)
+app.get("*", async (c) => {
+  const id = c.env.GAME.idFromName("A");
+  const obj = c.env.GAME.get(id);
+  const resp = await obj.fetch(c.req.url);
 
   if (resp.status === 404) {
-    return c.text('404 Not Found', 404)
+    return c.text("404 Not Found", 404);
   }
 
-  const count = parseInt(await resp.text())
-  return c.text(`Count is ${count}`)
-})
+  const count = parseInt(await resp.text());
+  return c.text(`Count is ${count}`);
+});
 
-export default app
+export default app;
